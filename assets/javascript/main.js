479db578f42bd920e31d6f226d8dbd59de9957c6
Yangyang He <heyyphilip@qq.com>
2018-10-02 07:34:54 -0400

    update button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,10 +2,11 @@ var PRIV_KEY = "0a40841836e0697f38a8f3e8362cd42c5184a9ec";
 var PUBLIC_KEY = "446bee50b1db8ed995dd97a138395991";
 
 function search() {
+  $('search_button').html('Searching..');
   var ts = new Date().getTime();
   var hash = md5(ts+PRIV_KEY + PUBLIC_KEY).toString();
   console.log(hash);
-  var url = 'https://gateway.marvel.com:80/v1/public/comics';
+  var url = 'http://gateway.marvel.com:80/v1/public/comics';
 
   var title = $('#seach_box').val();
   // var startYear = $('#startYear').val();
